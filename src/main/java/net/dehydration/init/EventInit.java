23428ac010c137e1c29e6c10e1b925ca3da159f2
Kevin Fischer <kevfischermen@gmail.com>
2023-08-29 23:06:25 -0700

    Zombies now drop potions (no effects)

--- src/main/java/net/dehydration/init/EventInit.java
+++ src/main/java/net/dehydration/init/EventInit.java
@@ -12,8 +12,12 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.item.Items;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTables;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.provider.number.BinomialLootNumberProvider;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.potion.PotionUtil;
+import net.minecraft.potion.Potions;
 import net.minecraft.registry.tag.BiomeTags;
 import net.minecraft.registry.tag.FluidTags;
 import net.minecraft.sound.SoundCategory;
@@ -27,6 +31,8 @@ import net.minecraft.util.math.BlockPos;
 
 public class EventInit {
 
+    private static final Identifier ZOMBIE_LOOT_TABLE_ID = new Identifier("minecraft", "entities/zombie");
+
     public static void init() {
         ServerEntityWorldChangeEvents.AFTER_PLAYER_CHANGE_WORLD.register((player, origin, destination) -> {
             ((ServerPlayerAccess) player).compatSync();
@@ -35,6 +41,15 @@ public class EventInit {
         LootTableEvents.MODIFY.register((resourceManager, lootManager, id, supplier, setter) -> {
             if (id.equals(new Identifier(LootTables.SPAWN_BONUS_CHEST.toString()))) {
                 LootPool pool = LootPool.builder().with(ItemEntry.builder(Items.GLASS_BOTTLE).build()).rolls(BinomialLootNumberProvider.create(5, 0.9F)).build();
+                supplier.pool(pool);
+            }
+            if (setter.isBuiltin() && ZOMBIE_LOOT_TABLE_ID.equals(id)){
+                // todo this does not actually add a thick potion to the zombie loot table yet. Just an uncraftable one.
+                var awkwardEntry = PotionUtil.setPotion(Items.POTION.getDefaultStack(), Potions.THICK).getItem();
+                LootPool pool = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.5F))
+                        .with(ItemEntry.builder(awkwardEntry)).build();
                 supplier.pool(pool);
             }
         });
